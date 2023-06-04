261bc28ff80dce1196ea801139b696340ee0be91
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-06-04 16:01:22 +1200

    save

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -31,11 +31,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
+    // connection: {
+    //   database: 'my_db',
+    //   user: 'username',
+    //   password: 'password',
+    // },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
